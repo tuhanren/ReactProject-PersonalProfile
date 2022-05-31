f2cf7d0210cf8d5ad4059ea3d6bbffc77a32473c
TuHanren <tu.hanren@gmail.com>
2022-05-31 12:26:26 -0400

    navMobile structured styled

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -61,6 +61,41 @@ const NavMobile = () => {
         animate={isOpen ? "visible" : "hidden"}
         className="w-4 h-4 rounded-full bg-accent fixed top-0 right-0"
       ></motion.div>
+      {/* menu */}
+      <motion.ul
+        variants={ulVariants}
+        initial="hidden"
+        animate={isOpen ? "visible" : ""}
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-full flex flex-col justify-center items-center transition-all duration-300 overflow-hidden`}
+      >
+        {/* top right close icon */}
+        <div
+          onClick={() => setIsOpen(false)}
+          className="cursor-pointer absolute top-8 right-8"
+        >
+          <XIcon className="w-8 h-8" />
+        </div>
+        {
+          // navigation items
+          navigation.map((item, index) => {
+            return (
+              <li key={index} className="mb-8">
+                <Link
+                  to={item.href}
+                  smooth={true}
+                  duration={500}
+                  offset={-70}
+                  className="text-xl cursor-pointer capitalize"
+                >
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })
+        }
+      </motion.ul>
     </nav>
   );
 };
